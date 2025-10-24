0ce3a7220a4326d5b03036ea5cb4e5141ae511e8
Seonggyun01 <thdrbs010314@naver.com>
2025-10-24 12:45:16 +0900

    feat(domain): Game 클래스 추가
    - 랜덤 값에 따라 자동차 전진

---
+++ src/main/java/racingcar/domain/Game.java
@@ -0,0 +1,21 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Game {
+    private static final int MOVABLE_LOWER_BOUND = 4;
+    private Cars cars;
+
+    public Game(Cars cars){
+        this.cars = cars;
+    }
+
+    public void play(){
+        for(Car car: cars.getCarList()){
+            if(Randoms.pickNumberInRange(0,9)>=MOVABLE_LOWER_BOUND){
+                car.move();
+            }
+        }
+    }
+
+}
