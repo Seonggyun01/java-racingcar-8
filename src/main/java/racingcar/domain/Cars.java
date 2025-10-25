ecbc2132936353d5f5f84c43c7b2fcbfc57f74cf
Seonggyun01 <thdrbs010314@naver.com>
2025-10-25 15:06:20 +0900

    feat(domain): 정적 팩토리 메서드, 우승자 이름 반환 기능 추가
    - List<String>으로 Cars 클래스 생성을 위하여 정적 팩토리 메서드 추가
    - 우승자 이름을 가져오는 기능 추가

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
     private List<Car> carList;
@@ -8,16 +9,29 @@ public class Cars {
     public Cars(List<Car> carList) {
         this.carList = carList;
     }
+    public static Cars fromNames(List<String> carNames){
+        List<Car> cars = carNames.stream()
+                .map(Car::new)
+                .toList();
+        return new Cars(cars);
+    }
 
     public Cars findWinners(){
         int max = carList.stream()
                 .mapToInt(Car::getPosition)
                 .max()
                 .getAsInt();
+
         List<Car> winners = carList.stream()
                 .filter(c-> c.getPosition() == max)
                 .toList();
+
         return new Cars(winners);
+    }
+
+    public String[] getWinnersName(){
+        return new String[]{carList.stream().
+                map(Car::getCarName).collect(Collectors.joining(" "))};
     }
 
     public List<Car> getCarList(){
