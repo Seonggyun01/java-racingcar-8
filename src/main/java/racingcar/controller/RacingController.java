fc3ed62dfd6bf0e2d7071e29b95ecbb4c6e32533
Seonggyun01 <thdrbs010314@naver.com>
2025-10-25 15:09:25 +0900

    feat(controller): RacingConroller 클래스 추가
    - 전체 실행 흐름 구현
    - 사용자 입력 받는 기능
    - 입력 받은 값으로 Cars, Count 객체 생성 기능
    - 경기 진행 및 중간 결과 출력 기능
    - 우승자 출력 기능

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,4 +1,79 @@
 package racingcar.controller;
 
+import java.util.List;
+import racingcar.domain.CarNameParser;
+import racingcar.domain.Cars;
+import racingcar.domain.Count;
+import racingcar.domain.Game;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class RacingController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public RacingController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run(){
+        String inputCarName;
+        String inputCount;
+
+        //사용자 입력 받기
+        inputCarName = getInputCarName();
+        inputCount = getInputCount();
+        //사용자 입력 (,)로 분리하기
+        List<String> carNames = CarNameParser.parse(inputCarName);
+        //Cars 객체 생성
+        Cars cars = Cars.fromNames(carNames);
+        //Count로 횟수 관리
+        Count count = new Count(inputCount);
+
+        //경기 시작 및 중간 결과 출력
+        racingGamePlay(cars, count);
+        //우승자 출력
+        winnerPrint(cars);
+    }
+
+    private void winnerPrint(Cars cars) {
+        Cars winners = cars.findWinners();
+        outputView.printWinners(winners.getWinnersName());
+    }
+
+    private void racingGamePlay(Cars cars, Count count) {
+        outputView.printResultMessage();
+        Game game = new Game(cars);
+        for(int i = 0; i< count.getCount(); i++){
+            game.play();
+            printRoundResult(cars);
+        }
+    }
+
+    private String getInputCount() {
+        String inputCount;
+        outputView.printCountPrompt();
+        inputCount = inputView.readCount();
+        if(inputCount==null||inputCount.isEmpty()){
+            throw new IllegalArgumentException("횟수를 입력해주세요.");
+        }
+        return inputCount;
+    }
+
+    private String getInputCarName() {
+        String inputCarName;
+        outputView.printCarNamesPrompt();
+        inputCarName = inputView.readCarNames();
+        if(inputCarName==null || inputCarName.isEmpty()){
+            throw new IllegalArgumentException("자동차 이름을 입력해주세요.");
+        }
+        return inputCarName;
+    }
+
+    private void printRoundResult(Cars cars) {
+        cars.getCarList().
+                forEach(car->outputView.printNameAndPosition(car.getCarName(), car.getPosition()));
+        outputView.printLine();
+    }
 }
