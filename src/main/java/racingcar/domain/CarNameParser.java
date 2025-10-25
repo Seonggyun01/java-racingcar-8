733de64c90b8da9ee2e8e9a57fa2189ba54f4d4f
Seonggyun01 <thdrbs010314@naver.com>
2025-10-25 15:03:33 +0900

    feat(domain): CarNameParser 클래스 추가
    - 자동차이름 문자열 쉼표(,) 기준으로 분리하는 기능

--- src/main/java/racingcar/domain/CarNameParser.java
+++ src/main/java/racingcar/domain/CarNameParser.java
@@ -1,4 +1,13 @@
 package racingcar.domain;
 
+import java.util.List;
+
 public class CarNameParser {
+    public CarNameParser() {}
+
+    public static List<String> parse(String rawCarnames) {
+
+        String[] parsedCarname = rawCarnames.split(",");
+        return List.of(parsedCarname);
+    }
 }
