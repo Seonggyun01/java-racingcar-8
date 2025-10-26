5c1ad57b2b34a06f9d31db795ee4c5e0520122c4
Seonggyun01 <thdrbs010314@naver.com>
2025-10-26 22:23:13 +0900

    refector(view): OutputView 매직리터럴 상수명 수정
    - CAR_NAMES_PROMPT => INPUT_CAR_NAMES_MESSAGE
    - TRY_COUNT_PROMPT_MESSAGE => INPUT_COUNT_MESSAGE
    - 일관된 패턴으로 수정, PROMPT와 MESSAGE는 유사한 의미이므로 하나만 사용

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,18 +1,18 @@
 package racingcar.view;
 
 public class OutputView {
-    private final String CAR_NAMES_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private final String TRY_COUNT_PROMPT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final String INPUT_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
     private final String RESULT_MESSAGE = "실행 결과";
     private final String WINNERS_MESSAGE_PREFIX = "최종 우승자 : ";
     private final String POSITION_SYMBOL = "-";
 
     public void printCarNamesPrompt(){
-        System.out.println(CAR_NAMES_PROMPT);
+        System.out.println(INPUT_CAR_NAMES_MESSAGE);
     }
 
     public void printCountPrompt(){
-        System.out.println(TRY_COUNT_PROMPT_MESSAGE);
+        System.out.println(INPUT_COUNT_MESSAGE);
     }
 
     public void printResultMessage(){
