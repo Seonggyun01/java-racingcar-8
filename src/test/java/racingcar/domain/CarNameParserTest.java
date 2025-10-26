dd957802c3a1dce63d5cb8a074acf648c4536c45
Seonggyun01 <thdrbs010314@naver.com>
2025-10-26 18:37:03 +0900

    test(domain): CarNameParserTest 테스트 추가
    - 빈 문자열("pobi,,jun","pobi,") 입력 테스트
    - 한글 이름 입력 테스트
    - 다양한 입력 개수(1, 2, 10개) 정상 파싱 테스트

--- src/test/java/racingcar/domain/CarNameParserTest.java
+++ src/test/java/racingcar/domain/CarNameParserTest.java
@@ -1,4 +1,71 @@
-import static org.junit.jupiter.api.Assertions.*;
+package racingcar.domain;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
 class CarNameParserTest {
-  
+    @Test
+    void 이름_한개_파싱_테스트(){
+        assertSimpleTest(()->{
+            String carName = "pobi";
+            List<String> result = CarNameParser.parse(carName);
+            assertThat(result).contains("pobi");
+        });
+    }
+
+    @Test
+    void 이름_2개_파싱_테스트(){
+        assertSimpleTest(()->{
+            String carName = "pobi,woni";
+            List<String> result = CarNameParser.parse(carName);
+            assertThat(result).contains("pobi", "woni");
+        });
+    }
+    @Test
+    void 이름_10개_파싱_테스트(){
+        assertSimpleTest(()->{
+            String carName = "a,b,c,d,ㄱ,ㄴ,ㄷ,ㄹ,1,100";
+            List<String> result = CarNameParser.parse(carName);
+            assertThat(result).contains("a","b","c","d","ㄱ","ㄴ","ㄷ","ㄹ","1","100");
+        });
+    }
+
+    @Test
+    void 이름에_빈문자열_테스트1(){
+        assertSimpleTest(()->{
+            String carName = "pobi,";
+            List<String> result = CarNameParser.parse(carName);
+            assertThat(result).contains("pobi","");
+        });
+    }
+
+    @Test
+    void 이름에_빈문자열_테스트2(){
+        assertSimpleTest(()->{
+            String carName = "pobi,,jun";
+            List<String> result = CarNameParser.parse(carName);
+            assertThat(result).contains("pobi","","jun");
+        });
+    }
+
+    @Test
+    void 한글_이름_입력_테스트1(){
+        assertSimpleTest(()->{
+            String carName = "가나다";
+            List<String> result = CarNameParser.parse(carName);
+            assertThat(result).contains("가나다");
+        });
+    }
+
+    @Test
+    void 한글_이름_입력_테스트2(){
+        assertSimpleTest(()->{
+            String carName = "가나다,라마";
+            List<String> result = CarNameParser.parse(carName);
+            assertThat(result).contains("가나다", "라마");
+        });
+    }
 }
