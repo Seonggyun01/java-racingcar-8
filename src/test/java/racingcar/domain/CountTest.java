ecbe8a03ec3046cdbae1698f9106c9f64f0a7949
Seonggyun01 <thdrbs010314@naver.com>
2025-10-26 21:47:22 +0900

    refector(domain): 사용하지 않는 import 제거

--- src/test/java/racingcar/domain/CountTest.java
+++ src/test/java/racingcar/domain/CountTest.java
@@ -3,9 +3,7 @@ package racingcar.domain;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
-import com.sun.nio.sctp.IllegalReceiveException;
 import org.junit.jupiter.api.Test;
 
 class CountTest {
